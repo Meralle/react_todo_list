d08adc4c114f63a2392b0d18ba9cb3ad7338c53a
Elbehary <ahmedelbehary0@gmail.com>
2018-04-25 10:20:28 +0200

    create a intialize form

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import CreatePost from './Components/CreatePost';
+import React from 'react';
+// import logo from './logo.svg'; 
 
-class App extends Component {
+import './App.css';
+import CreatePost from './Components/CreatePost.js';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = localStorage.getItem("todos")
+
+    console.log(this.state);
+
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        
-      <CreatePost />
-         </div>
+      <React.Fragment>
+        <div className="container">
+          <div className="col s6">
+            <h1>Reactish-Todolist</h1>
+            <p className="lead">React with features add, change text, remove and filter by state.</p>
+              <CreatePost />
+            <h1>Todos:</h1>
+            <small className="text-success">
+              <a className="text-success mr-1" href="?state=false">show open</a>
+              <a className="text-success mr-1" href="?state=true">show done</a>
+              <a className="text-success mr-1" href="./index.html">show all</a>
+            </small>
+            <ul className="list-group">
+              <li id="template" className="bg-dark list-group-item d-none justify-content-between align-items-center">
+                {/* <button className="btn btn-sm checkbutton btn-outline-success">done</button> 
+                      <span contentEditable="true" className="title">Cras justo odio</span> 
+                      <button className="btn btn-sm btn-outline-danger">X</button> */}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
 
-export default App;
+export default App; 
--- src/Components/CreatePost.js
+++ src/Components/CreatePost.js
@@ -21,14 +21,16 @@ class CreatePost extends Component {
 
   render() {
   	return(
-  		<div>
-  			<form onSubmit={this.addTodo}> 
-					<input ref={(e) => this._inputElement = e} placeholder="enter task"/>
-					<button className="btn waves-effect waves-light"  type="submit">add</button>
-				</form>	
-  		</div>
+			<div className="container" >
+				<label htmlFor="todo">Add Todo:</label>
+				<form action="#" className="form-inline mb-5 row">
+					<div className="col s8">
+						<input type="text" className="form-control mr-2 w-100" defaultValue="milk" id="input" aria-describedby="emailHelp" name="input" placeholder="enter task" ref={(e) => this._inputElement = e} />
+					</div>
+					<button type="submit" className="btn btn-success">Submit</button>
+				</form>
+			</div>
   	)
-
   }
 }
 export default CreatePost;
