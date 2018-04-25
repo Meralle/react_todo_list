7ad75ac0dcdf5ab3ac1a048c0831a37698388be4
h <mar.ha17@outlook.com>
2018-04-26 00:01:26 +0200

    styled the TodoList

--- src/Components/CreatePost.jsx
+++ src/Components/CreatePost.jsx
@@ -8,30 +8,41 @@ class CreatePost extends React.Component {
 		}
 	}
   render() {
+  	const style={
+  		font:{
+  			font:"16px"
+  		},
+  		 margin:"0 1rem"
+  		
+
+  	}
 		const templatelist = this.props.todos.map((item, i) =>{
-			return item.text !== '' ?  (
-					<li className="" data-name="meral" key={i}>
-						<button className="">done</button>
-						<span className="title">{item.text}</span>
-						<button className="" onClick={(e) => this.props.handleDelete(item)}>X</button>
-					</li>
-					) : (null)
+			return item.text !== '' ?  (			
+			      		<ul className="collection with-head" data-name="meral" key={i}>
+					        <li onClick={(e) => this.props.handleDelete(item)} style={{margin: 3 + 'em'}} >
+					        	<a className="waves-effect btn orange darken-4 btn-small right">delete</a>
+					        </li>
+					        	<span className="title">{item.text}</span>
+					        	<a className="waves-effect  btn orange darken-4 btn-small right"  style={style}>done</a>
+			      		</ul>
+					) : (null)	
   
 })
   	return(
 			<div className="container" >
+			<h1 style={{marginRight: 12 + 'em'}} ><strong>Todo List</strong></h1>
 				<form action="#" className="addTodo form-inline mb-5 row">
 					<div className="col s8">
-						<label htmlFor="todo">Add Todo:</label>
-						<input type="text" value={this.props.value} className="form-control mr-2 w-100" id="input" aria-describedby="emailHelp" placeholder="enter task" name="input" onChange={this.props.inputName}/>
+						
+						<input style={style.font} type="text" value={this.props.value} className="form-control mr-2 w-100" id="input" aria-describedby="emailHelp" placeholder="enter task" name="input" onChange={this.props.inputName}/>
 					</div>
 					<div className="col s8">
-					<button className="btn waves-effect waves-light" type="submit" onClick={this.props.saveInput}>
+					<button className="btn waves-effect orange darken-4 right" type="submit" onClick={this.props.saveInput}>
 						Submit
           			</button>
 					</div>
 				</form>
-				<h1><strong>Todo List</strong></h1>
+				
 				<ul>{templatelist}</ul>
 			</div>
   	)
