02ab667c10e6119b525cea325bfe5f27b860dbde
Meralle <mar.ha17@outlook.com>
2018-04-25 14:39:06 +0200

    WIP

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,8 @@ class App extends React.Component {
       todos:JSON.parse(localStorage.getItem("todos")) || [],
       inputdata:""
     }
-      this.inputName = this.inputName.bind(this); 
-      this.saveinput = this.saveinput.bind(this);
   }
   
-    inputName =(e) => {
-      let inputdata = this.state.inputdata
-      let update = e.target.value
-      this.setState({inputdata: update});
-    }
 
     saveinput = (e)=> {
       var todosCopy = [...this.state.todos];
@@ -67,6 +60,7 @@ class App extends React.Component {
   this.setState({ todos })
   }
 
+   
   render() {
     return (
       <div className="App">
