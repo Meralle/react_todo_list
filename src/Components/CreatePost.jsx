1870ba4355c9e7d842738317ee89c16b7aefc22f
h <mar.ha17@outlook.com>
2018-04-26 13:12:03 +0200

    modified the form and style it

--- src/Components/CreatePost.jsx
+++ src/Components/CreatePost.jsx
@@ -17,36 +17,36 @@ class CreatePost extends React.Component {
 
   	}
 		const templatelist = this.props.todos.map((item, i) =>{
-			return item.text !== '' ?  (			
-			      		<ul className="collection with-head" data-name="meral" key={i}>
-					        <li onClick={(e) => this.props.handleDelete(item)} style={{margin: 3 + 'em'}} >
-					        	<a className="waves-effect btn orange darken-4 btn-small right">delete</a>
-					        </li>
-					        	<span className="title">{item.text}</span>
-					        	<a className="waves-effect  btn orange darken-4 btn-small right"  style={style}>done</a>
-			      		</ul>
+			return item.text !== '' ?  (				
+		        <li className="collection with-head" data-name="meral" key={i} onClick={(e) => this.props.handleDelete(item)}>
+		        	<a className="waves-effect btn orange darken-4 btn-small right">delete</a>
+		        	<a className="waves-effect  btn orange darken-4 btn-small right">done</a>
+		        	<h3 className="title">{item.text}</h3>
+		        </li>	
+			      		
 					) : (null)	
   
 })
   	return(
-			<div className="container" >
-			<h1 style={{marginRight: 12 + 'em'}} ><strong>Todo List</strong></h1>
-				<form action="#" className="addTodo form-inline mb-5 row">
-					<div className="col s8">
-						
-						<input style={style.font} type="text" value={this.props.value} className="form-control mr-2 w-100" id="input" aria-describedby="emailHelp" placeholder="enter task" name="input" onChange={this.props.inputName}/>
+			<div className="container row" >
+				<h1><strong>Todo List</strong></h1>
+					<div className="form">
+					<form action="#" className="addTodo form-inline row">
+						<input style={style.font} type="text" value={this.props.value} className="" id="input" aria-describedby="emailHelp" placeholder="enter task" name="input" onChange={this.props.inputName}/>
+						<br/>
+						<button className="btn waves-effect orange darken-4 right" type="submit" onClick={this.props.saveInput}>
+							Submit
+		          		</button>
+					</form>
 					</div>
-					<div className="col s8">
-					<button className="btn waves-effect orange darken-4 right" type="submit" onClick={this.props.saveInput}>
-						Submit
-          			</button>
+					<div className="row">
+						<ul>{templatelist}</ul>
 					</div>
-				</form>
-				
-				<ul>{templatelist}</ul>
 			</div>
   	)
   }
 }
 export default CreatePost;
-  
+
+
+  {/*<li class="collection-item"><div>Alvin<a href="#!" class="secondary-content"><i class="material-icons">send</i></a></div></li>*/}
