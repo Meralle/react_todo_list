cd0603823eee18a922b72daf74139f39169f0416
h <mar.ha17@outlook.com>
2018-04-28 15:07:49 +0200

    modified methods name

--- src/Components/CreatePost.jsx
+++ src/Components/CreatePost.jsx
@@ -21,7 +21,7 @@ class CreatePost extends React.Component {
 					<form action="#" className="addTodo form-inline row">
 						<input type="text" value={this.props.value} aria-describedby="emailHelp" placeholder="enter task" name="input" onChange={this.props.changeInput}/>
 						<br/>
-						<button className="btn waves-effect orange darken-3 right" type="submit" onClick={this.props.saveInput}>
+						<button className="btn waves-effect orange darken-3 right" type="submit" onClick={this.props.handleAddTodo}>
 							Submit
 		          		</button>
 					</form>
