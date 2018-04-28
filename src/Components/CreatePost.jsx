8acf802eab84cada98a03c13f89bf2af126d4c3e
h <mar.ha17@outlook.com>
2018-04-28 19:25:45 +0200

    modified the code

--- src/Components/CreatePost.jsx
+++ src/Components/CreatePost.jsx
@@ -5,11 +5,8 @@ class CreatePost extends React.Component {
 
 	}
   render() {
-  		let filterTodos =this.props.todos.filter( item => {
-  			return item.text.indexOf(this.props.todos) !== -1;
-  		})
 		const templatelist = this.props.todos.length > 0 ?  (
-		filterTodos.map((item, i) =>
+		this.props.todos.map((item, i) =>
 		        (<li className="collection with-head" id={item._id} key={i} >
 		        	<a className="waves-effect btn orange darken-3 btn-small right" onClick={(e) => this.props.handleDelete(item)}>delete</a>
 		        	<a className={"waves-effect  btn  btn-small right"  + (item.completed ? ' orange darken-3' : '  lime accent-3')} onClick={() => this.props.handleCompleted(item._id)}>done</a>
