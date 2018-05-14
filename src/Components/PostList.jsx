a5256b2eeb8f59cce248994438f32abc4ef3647d
Meralle <mar.ha17@outlook.com>
2018-05-15 00:34:08 +0200

    fixed completed indo + styled the input

--- src/Components/PostList.jsx
+++ src/Components/PostList.jsx
@@ -8,7 +8,7 @@ class PostList extends React.Component {
 			 filterTodos.map((item, i) =>
 		        (<li  id={item._id} key={i} className={"collection with-head" + (item.completed ? ' lime accent-1' : '')} >
 		        	<a className="waves-effect btn orange darken-3 btn-small right" onClick={(e) => this.props.handleDelete(item)}>delete</a>
-		        	<a className={"waves-effect  btn  btn-small right"  + (item.completed ? ' lime accent-3' : '  orange darken-3')} onClick={() => this.props.handleCompleted(item._id)}>{item.completed ? 'undo' : 'done' }</a>
+		        	<a className={"waves-effect  btn  btn-small right"  + (item.completed ? ' lime accent-3' : '  orange darken-3')} onClick={() => this.props.handleCompleted(item._id)}>{item.completed ? 'done' : 'undo' }</a>
 		        	<h3 className={"title" +(item.completed ? ' light-green lighten-1' : '')}>{item.text}</h3>
 		        </li>)
 			      		
